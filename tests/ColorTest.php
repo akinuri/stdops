bbe1ce65803a8c639a977a07fffe328944e3cc51
Noreh AD <akinoreh@gmail.com>
2025-05-01 16:56:00 +0300

    (test) ColorTest::parseHslString(): simple cases

--- tests/ColorTest.php
+++ tests/ColorTest.php
@@ -13,7 +13,63 @@ final class ColorTest extends CustomTestCase
     {
         $cases = [
             [
+                "name" => "no arg",
+                "args" => [],
+                "throws" => ArgumentCountError::class,
+            ],
+            [
+                "name" => "invalid arg",
+                "args" => [123],
+                "throws" => InvalidArgumentException::class,
+            ],
+            [
+                "name" => "empty string",
+                "args" => [""],
+                "throws" => InvalidArgumentException::class,
+            ],
+            [
+                "name" => "unexpected string",
+                "args" => ["lorem ipsum"],
+                "throws" => InvalidArgumentException::class,
+            ],
+            [
                 "args" => ["hsl(120,50%,75%)"],
+                "expected" => [
+                    "hue" => 120,
+                    "sat" => 50,
+                    "lum" => 75,
+                    "alpha" => 1.0,
+                ],
+            ],
+            [
+                "args" => ["hsl(120, 50%, 75%)"],
+                "expected" => [
+                    "hue" => 120,
+                    "sat" => 50,
+                    "lum" => 75,
+                    "alpha" => 1.0,
+                ],
+            ],
+            [
+                "args" => ["hsl(120deg, 50%, 75%)"],
+                "expected" => [
+                    "hue" => 120,
+                    "sat" => 50,
+                    "lum" => 75,
+                    "alpha" => 1.0,
+                ],
+            ],
+            [
+                "args" => ["hsl(120deg 50% 75%)"],
+                "expected" => [
+                    "hue" => 120,
+                    "sat" => 50,
+                    "lum" => 75,
+                    "alpha" => 1.0,
+                ],
+            ],
+            [
+                "args" => ["hsl(120 50 75)"],
                 "expected" => [
                     "hue" => 120,
                     "sat" => 50,
