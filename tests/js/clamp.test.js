7bd0a8f6d3658e5c6ade54a39208b0986f47e3d7
Noreh AD <akinoreh@gmail.com>
2025-05-17 22:26:16 +0300

    (test) js/clamp: more cases

--- tests/js/clamp.test.js
+++ tests/js/clamp.test.js
@@ -3,5 +3,9 @@ const { load } = require("./test-utils");
 load("math/js/clamp.js");
 
 test("clamp", () => {
-    expect(clamp(2, 0, 10)).toBe(2);
+    expect(clamp(-1, 0, 10)).toBe(0);
+    expect(clamp(0, 0, 10)).toBe(0);
+    expect(clamp(3, 0, 10)).toBe(3);
+    expect(clamp(10, 0, 10)).toBe(10);
+    expect(clamp(11, 0, 10)).toBe(10);
 });
